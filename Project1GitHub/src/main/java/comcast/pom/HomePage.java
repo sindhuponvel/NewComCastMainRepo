5e12e621b7b7d0ad6cbbc6322aa1222ea03374f5
Chethan1306 <166906416+Chethan1306@users.noreply.github.com>
2024-05-28 16:37:36 +0530

    Added home page

--- Project1GitHub/src/main/java/comcast/pom/HomePage.java
+++ Project1GitHub/src/main/java/comcast/pom/HomePage.java
@@ -1,5 +1,19 @@
 package comcast.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+	WebDriver d;
+	public HomePage(WebDriver d)
+	{
+		this.d=d;
+		PageFactory.initElements(d,this);
+	}
+	
+	@FindBy(xpath="//a[text()='Products']")
+	private WebElement productLink;
 
 }
